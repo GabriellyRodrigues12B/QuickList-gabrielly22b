3ee574eb4ed91561002142e6b5dca09169740d6b
Gabrielly Rodrigues 12b <contadagabiparaestudosnaunasp@gmail.com>
2025-06-01 23:19:58 -0300

    criação da função chekItem para marcar e desmarcar itens do array

--- main.js
+++ main.js
@@ -23,11 +23,11 @@ function showItemsList(){
         sectionList.innerHTML += `
         <div class="item">
             <div>
-                <input type="checkbox" name="List" id="item-${index}">
-                <div class="custom-checkbox">
+                <input type="checkbox" name="List" id="item-${index}" ${item.checked ? "checked" : ""}>
+                <div class="custom-checkbox" onclick="checkItem('${item.name}')">
                     <img src="./assets/checked.svg" alt="checked">
                 </div>
-                <label for="item-${index}">${item.name}</label>
+                <label for="item-${index}" onclick="checkItem('${item.name}')">${item.name}</label>
             </div>
             <button onclick="removeItem('${item.name}')">
                 <img src="./assets/trash-icon.svg" alt="trash-icon">
@@ -39,14 +39,12 @@ function showItemsList(){
 
 function removeItem(itemName){
     const itemIndex = items.findIndex((item) => item.name === itemName)
-    const divWarning =document.querySelector(".warning")
+    const divWarning = document.querySelector(".warning")
 
     divWarning.classList.remove("hide-warning")
 
-    setTimeout(() =>{
-
+    setTimeout(() => {
         divWarning.classList.add("hide-warning")
-
     }, 4000)
 
     if (itemIndex !== -1){
@@ -57,7 +55,15 @@ function removeItem(itemName){
 }
 
 function addHideWarningClass(){
-
-
     document.querySelector(".warning").classList.add("hide-warning")
+}
+
+function checkItem(itemName){
+    const item = items.find((item) => item.name === itemName)
+
+    if (item) {
+        item.checked = !item.checked
+    }
+
+    showItemsList()
 }
